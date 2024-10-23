9cc4973d7f9059a8af8b1542c1ebd5476480fd80
cnvrt <60330723+cnvrt@users.noreply.github.com>
2024-10-23 05:48:16 +0000

    update

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,3 @@
-const navbar = document.querySelector(".dropdown");
-function hamburg(){
-    navbar.style.transform = "translateY(0px)";
-}
-
-function cancel() {
-    navbar.style.transform = "translateY(-500px)";
-}
-
 //for Typewriter effect
 
 const texts = [
